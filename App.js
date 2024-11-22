07efe3423cbf2a5f071da74ad07a0d26f0a8d881
Namrata Chandarana <namratachandarana20@gmail.com>
2024-11-22 17:53:38 +0530

    offlineNotifier added

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home, Cart, RestautrantMenu, About} from "./src/pages";
 import Header from "./src/components/Header";
+import OfflineNotifier from "./src/components/offlineNotifier";
 
 
 function App(){
     return (
         <Router>
+          
           <Header/>
+          <OfflineNotifier/>
           <Routes>
             <Route path="/" element={
               <Home />
@@ -21,7 +24,6 @@ function App(){
             <Route path="/about" element={
               <About />
             } />
-
           </Routes>
         </Router>
       );
---
+++ src/components/OfflineNotifier.jsx
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from 'react'
+
+const OfflineNotifier = () => {
+    const [isOffline, setIsOffline] = useState(!navigator.onLine)
+    console.log(navigator)
+    console.log(navigator.onLine)
+
+    useEffect(() => {
+      const handleOnline = () => setIsOffline(false);
+      const handleOffline = () => setIsOffline(true);  
+
+      window.addEventListener('online', handleOnline)
+      window.addEventListener('offline', handleOffline)
+    
+      return () => {
+        window.removeEventListener('online', handleOnline);
+        window.removeEventListener('offline', handleOffline);
+      }
+    }, [])
+    console.log(isOffline)
+    
+  return (
+    !isOffline && (
+        <div className="fixed top-0 left-0 w-full bg-red-700 text-white text-center py-2 z-50">
+            You are offline. Please check your internet connection.
+        </div>
+    )
+  )
+}
+
+export default OfflineNotifier;
