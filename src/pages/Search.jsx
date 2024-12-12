9b26625403c1206f0feeb25a147be5f420a1d31f
Namrata Chandarana <namratachandarana20@gmail.com>
2024-12-12 07:39:17 +0530

    search items changes

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -22,18 +22,21 @@ const Search = () => {
     <div className='mt-10'>
         <div className='mx-5 sm:mx-10 md:mx-20 lg:mx-80 '>
             <input type="text" value={searchInput} autoFocus onChange={(e) => debounceHandler(e.target.value)} className='py-3 px-2 w-full outline-none rounded-sm border border-1 border-gray-300' placeholder='Search for restaurant... ' />
-            <div className='h-80'>
+            <div className='h-auto'>
                     {searchData.length > 0 ? (
                         searchData.map((res, index) => {
                             if(res.type === "RESTAURANT") {
                                 const metadata = JSON.parse(res.metadata);
                                 const primaryRestaurantId = metadata.data?.primaryRestaurantId;
                                 return(
-                                    <Link to={`/productmenu?page-type="REGULAR_MENU&complete-menu=true&lat=${lat}&lng=${lon}&restaurantId=${primaryRestaurantId}&catalog_qa="undefined"&submitAction="ENTER"`}>
+                                     <Link to={`/productmenu?page-type="REGULAR_MENU&complete-menu=true&lat=${lat}&lng=${lon}&restaurantId=${primaryRestaurantId}&catalog_qa="undefined"&submitAction="ENTER"`}>
                                         <div className='hover:bg-gray-100 py-5'>
                                             <div className='flex space-x-4'>
-                                                <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/${res.cloudinaryId}`} alt="" className='h-16' />
-                                                <h1 className='py-3 align-bottom'>{res.text}</h1>
+                                                <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/${res.cloudinaryId}`} alt="" className='h-20 rounded-md' />
+                                                <div className='mt-5'>
+                                                    <h1 className=' align-bottom'>{res.text}</h1>
+                                                    <h1 className='text-lightBlue align-bottom'>Restaurant</h1>
+                                                </div>
                                             </div>
                                         </div>
                                     </Link>
